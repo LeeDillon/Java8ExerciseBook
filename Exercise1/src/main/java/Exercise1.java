a35290afd70412767cc8f86e7359201759491004
Lee-Dillon <lee.dillon1@outlook.com>
2021-08-24 09:55:26 +0100

    completed exercise 1

---
+++ Exercise1/src/main/java/Exercise1.java
@@ -0,0 +1,16 @@
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println(hello());
+	}
+	
+	public static String hello() {
+		return "Hello World";
+		
+	}
+
+}
